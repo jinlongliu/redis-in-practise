3edf1275c7f4e31e0bdeb043e4b0503ff74b91b8
Liu Jinlong <nnuljl@gmail.com>
2016-11-23 18:31:54 +0800

    控制器处理时，新增Reids订阅

--- src/main/java/top/onos/controller/IndexController.java
+++ src/main/java/top/onos/controller/IndexController.java
@@ -1,8 +1,14 @@
 package top.onos.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.listener.PatternTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -11,14 +17,31 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Autowired
+    private RedisMessageListenerContainer redisMessageListenerContainer;
+
+    @Autowired
+    private MessageListenerAdapter messageListenerAdapter;
+
     @RequestMapping("/")
     public String index() {
         return "index";
     }
 
-    @RequestMapping("/chat")
-    public String chat() {
+    @RequestMapping("/chatroom")
+    public String chatRoom() {
+        stringRedisTemplate.opsForValue().set("key1", "ssssss");
         return "chat";
+    }
+
+    @RequestMapping("/chatp2p/{uid}")
+    public String chatP2P(@PathVariable String uid) {
+        redisMessageListenerContainer.addMessageListener(messageListenerAdapter,
+                                                         new PatternTopic(uid));
+        return "chatP2P";
     }
 
 }
