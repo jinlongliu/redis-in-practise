ee79aae7d654dc2bae91243346a9195153702d64
Liu Jinlong <nnuljl@gmail.com>
2016-11-23 23:31:43 +0800

    新增相关控制处理方法。

--- src/main/java/top/onos/controller/IndexController.java
+++ src/main/java/top/onos/controller/IndexController.java
@@ -2,14 +2,14 @@ package top.onos.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.listener.PatternTopic;
+import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by Liu on 2016/11/22.
@@ -33,15 +33,29 @@ public class IndexController {
 
     @RequestMapping("/chatroom")
     public String chatRoom() {
-        stringRedisTemplate.opsForValue().set("key1", "ssssss");
+        stringRedisTemplate.opsForValue().set("keyTest", "valueTest");
         return "chat";
     }
 
     @RequestMapping("/chatp2p/{uid}")
     public String chatP2P(@PathVariable String uid) {
         redisMessageListenerContainer.addMessageListener(messageListenerAdapter,
-                                                         new PatternTopic(uid));
+                                                         new ChannelTopic(uid));
         return "chatP2P";
+    }
+
+    @RequestMapping("/admin")
+    public String admin() {
+        /*群发页面*/
+        return "admin";
+    }
+
+    @RequestMapping(value = "/sendAll", method = RequestMethod.POST)
+    @ResponseBody
+    public String sendAll(String message) {
+        /*接收请求，处理操作Redis向相关主题发送消息*/
+        stringRedisTemplate.convertAndSend("chat-*", message);
+        return "SUCCESS";
     }
 
 }
